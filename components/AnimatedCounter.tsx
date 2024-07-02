3c92525d58c13ca7727fc464d682fcc6784f6481
Jean Philippe Auguste <123325152+philippe2023@users.noreply.github.com>
2024-07-02 15:32:17 +0200

    home page setup, installed chart.js, react count-up

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react'
+import { formatAmount } from '@/lib/utils'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({ amount }: {amount:number }) => {
+  return (
+    <div className='w-full'>
+        <CountUp 
+        duration={2.75}
+        decimals={2}
+        decimal='.'
+        prefix='€ '
+        end={amount} />
+    </div>
+  )
+}
+
+export default AnimatedCounter
